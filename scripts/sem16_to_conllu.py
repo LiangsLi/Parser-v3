4dad46be9d598db5e0687d4abcafd921f1a67e7c
HITalexwang <hitalexwang@gmail.com>
2018-11-16 04:27:32 -0800

    fix sem16toconllu

--- scripts/sem16_to_conllu.py
+++ scripts/sem16_to_conllu.py
@@ -1,16 +1,19 @@
 from collections import defaultdict
+import codecs
 
 root = 'root'
 #===============================================================
 def to_conllu(sdp_filename, conllu_filename):
   """"""
 
-  with open(sdp_filename) as f:
+  with codecs.open(sdp_filename, 'r', "utf-8") as f:
     with open(conllu_filename, 'w') as g:
       buff = []
       sents = f.read().strip().split('\n\n')
-      #print ("sents: ", len(sents))
+      print (sents)
+      print ("sents: ", len(sents))
       for sent in sents:
+        #print (sent)
         conllu_form = []
         words = []
         lines = sent.strip().split('\n')
@@ -29,7 +32,7 @@ def to_conllu(sdp_filename, conllu_filename):
           #  print "Error:{}".format(line)
         for word in words:
           conllu_form.append('\t'.join(word))
-        g.write('\n'.join(conllu_form) + '\n\n')
+        g.write('\n'.join(conllu_form).encode("utf-8") + '\n\n')
 
 #***************************************************************
 if __name__ == '__main__':
